701ac381cec453b2df9ff020f2efbbeb6d5310b4
tee <tkaeophian@gmail.com>
2023-03-07 17:50:04 +0000

    implement single book endpoint

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const express = require('express')
 const { connectToDb, getDb } = require('./db')
+const { ObjectId } = require('mongodb')
 
 // init app & middleware
 const app = express()
 
 // db connection
-let dbConnection
+let db
 
 connectToDb((error) => {
     if (!error) {
         app.listen(3000, () => {
             console.log("app listening on port 3000")
         })
-        dbConnection = getDb()
+        db = getDb()
     }
 })
 
@@ -20,5 +21,32 @@ connectToDb((error) => {
 
 // routes
 app.get('/books', (req, res) => {
-  res.json({msg: "welcome to the api"})
+
+    let books = [];
+
+    db.collection('books')
+        .find()
+        .sort({author: 1})
+        .forEach(book => books.push(book))
+        .then(() => {
+            res.status(200).json(books)
+        }).catch((error) => {
+            res.status(500).json({error: 'could not fetch the document'})          
+        })
+})
+
+app.get('/books/:id', (req, res) => {
+    const { id } = req.params
+    
+    if (!ObjectId.isValid(id)) {
+        res.status(400).json({error: 'invalid document id'})          
+    }
+
+    db.collection('books')
+        .findOne({ _id: new ObjectId(id) })
+        .then(doc => {
+            res.status(200).json(doc)
+        }).catch((error) => {
+            res.status(500).json({error: 'could not fetch the document'})          
+        })
 })
